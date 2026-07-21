562ab737b8dfe90d4874e7f6aeb7f19a441ea748
ggldnl <danielgigliotti99.dg@gmail.com>
2026-07-21 23:12:09 +0200

    Updated asset zoo README

--- src/mjlab/asset_zoo/README.md
+++ src/mjlab/asset_zoo/README.md
@@ -1,8 +1,12 @@
 # Asset Zoo
 
-| Robot           | Image                                                                                                         |
-|-----------------|---------------------------------------------------------------------------------------------------------------|
-| **Unitree G1**  | <img src="../../../docs/source/_static/content/g1.png" width="200" height="150" alt="Unitree G1 Humanoid">    |
-| **Unitree Go1** | <img src="../../../docs/source/_static/content/go1.png" width="200" height="150" alt="Unitree Go1 Quadruped"> |
-| **i2rt YAM**    | <img src="../../../docs/source/_static/content/yam.png" width="200" height="150" alt="i2rt YAM Robot Arm">    |
-| **Booster T1**  | <img src="../../../docs/source/_static/content/t1.png" width="200" height="150" alt="Booster T1">             |
+| Robot            | Image                                                                                                         |
+|------------------|---------------------------------------------------------------------------------------------------------------|
+| **Unitree G1**   | <img src="../../../docs/source/_static/content/g1.png" width="200" height="150" alt="Unitree G1 Humanoid">    |
+| **Unitree Go1**  | <img src="../../../docs/source/_static/content/go1.png" width="200" height="150" alt="Unitree Go1 Quadruped"> |
+| **i2rt YAM**     | <img src="../../../docs/source/_static/content/yam.png" width="200" height="150" alt="i2rt YAM Robot Arm">    |
+| **Booster T1**   | <img src="../../../docs/source/_static/content/t1.png" width="200" height="150" alt="Booster T1">             |
+| **KUKA IIWA 14** | <img src="../../../docs/source/_static/content/iiwa_14.png" width="200" height="150" alt="KUKA IIWA 14">      |
+| **Crawler**      | <img src="../../../docs/source/_static/content/crawler.png" width="200" height="150" alt="Crawler">           |
+| **Cartpole**     | Classic control toy: a cart on a rail with a free-swinging pole.                                              |
+| **Diffdrive**    | Two-wheel differential drive robot.                                                                           |
--- src/mjlab/asset_zoo/robots/__init__.py
+++ src/mjlab/asset_zoo/robots/__init__.py
@@ -1,21 +1,4 @@
-from mjlab.asset_zoo.robots.i2rt_yam.yam_constants import (
-  YAM_ACTION_SCALE as YAM_ACTION_SCALE,
-)
-from mjlab.asset_zoo.robots.i2rt_yam.yam_constants import (
-  get_yam_robot_cfg as get_yam_robot_cfg,
-)
-from mjlab.asset_zoo.robots.unitree_g1.g1_constants import (
-  G1_ACTION_SCALE as G1_ACTION_SCALE,
-)
-from mjlab.asset_zoo.robots.unitree_g1.g1_constants import (
-  get_g1_robot_cfg as get_g1_robot_cfg,
-)
-from mjlab.asset_zoo.robots.unitree_go1.go1_constants import (
-  GO1_ACTION_SCALE as GO1_ACTION_SCALE,
-)
-from mjlab.asset_zoo.robots.unitree_go1.go1_constants import (
-  get_go1_robot_cfg as get_go1_robot_cfg,
-)
+# Booster T1
 from mjlab.asset_zoo.robots.booster_t1.t1_constants import (
   T1_ACTION_SCALE as T1_ACTION_SCALE,
 )
@@ -23,20 +6,67 @@ from mjlab.asset_zoo.robots.booster_t1.t1_constants import (
   get_t1_robot_cfg as get_t1_robot_cfg,
 )
 
+# Cartpole
+from mjlab.asset_zoo.robots.cartpole.cartpole_constants import (
+  get_cartpole_robot_cfg as get_cartpole_robot_cfg,
+)
+from mjlab.asset_zoo.robots.crawler.actuators import (
+  ACTION_OFFSET as ACTION_OFFSET,
+)
+
 # Crawler
 from mjlab.asset_zoo.robots.crawler.actuators import (
   ACTION_SCALE as ACTION_SCALE,
-  ACTION_OFFSET as ACTION_OFFSET,
 )
-from mjlab.asset_zoo.robots.crawler.sensors import (
-  IMU as IMU,
-  SENSORS as SENSORS,
+from mjlab.asset_zoo.robots.crawler.collisions import (
+  BASE_NAME as BASE_NAME,
+)
+from mjlab.asset_zoo.robots.crawler.collisions import (
+  FOOT_GEOM_NAMES as FOOT_GEOM_NAMES,
 )
 from mjlab.asset_zoo.robots.crawler.collisions import (
   FOOT_SITE_NAMES as FOOT_SITE_NAMES,
-  FOOT_GEOM_NAMES as FOOT_GEOM_NAMES,
-  BASE_NAME as BASE_NAME,
 )
 from mjlab.asset_zoo.robots.crawler.crawler_constants import (
   get_crawler_robot_cfg as get_crawler_robot_cfg,
+)
+from mjlab.asset_zoo.robots.crawler.sensors import (
+  IMU as IMU,
+)
+from mjlab.asset_zoo.robots.crawler.sensors import (
+  SENSORS as SENSORS,
+)
+
+# Diffdrive
+from mjlab.asset_zoo.robots.diffdrive.diffdrive_constants import (
+  get_diffdrive_robot_cfg as get_diffdrive_robot_cfg,
+)
+
+# I2RT YAM arm
+from mjlab.asset_zoo.robots.i2rt_yam.yam_constants import (
+  YAM_ACTION_SCALE as YAM_ACTION_SCALE,
+)
+from mjlab.asset_zoo.robots.i2rt_yam.yam_constants import (
+  get_yam_robot_cfg as get_yam_robot_cfg,
+)
+
+# KUKA iiwa14 arm
+from mjlab.asset_zoo.robots.kuka_iiwa_14.kuka_constants import (
+  get_kuka_robot_cfg as get_kuka_robot_cfg,
+)
+
+# Unitree G1
+from mjlab.asset_zoo.robots.unitree_g1.g1_constants import (
+  G1_ACTION_SCALE as G1_ACTION_SCALE,
+)
+from mjlab.asset_zoo.robots.unitree_g1.g1_constants import (
+  get_g1_robot_cfg as get_g1_robot_cfg,
+)
+
+# Unitree GO1
+from mjlab.asset_zoo.robots.unitree_go1.go1_constants import (
+  GO1_ACTION_SCALE as GO1_ACTION_SCALE,
+)
+from mjlab.asset_zoo.robots.unitree_go1.go1_constants import (
+  get_go1_robot_cfg as get_go1_robot_cfg,
 )
